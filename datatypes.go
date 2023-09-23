6528bd11f8a665aa60fb424fa74b7a899bc77bdd
Muthonikiboi <kiboijoye254@gmail.com>
2023-09-23 23:51:56 +0300

    datatypes

---
+++ datatypes.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main() {
+	// integer
+	var number int =2
+	fmt.Println(number)
+
+	//boolean
+	var boolean bool= true
+	fmt.Println(boolean)
+
+	//string
+	var sentence string = "Emmanuel is a Software Engineer"
+	fmt.Println(sentence)
+
+	//float
+	var float1 float32
+	float1 = 56.098438905
+	fmt.Println(float1)
+}
