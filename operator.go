6220c3df5449be0876530b69fc54502c32dcc7a5
Muthonikiboi <kiboijoye254@gmail.com>
2023-09-25 22:23:58 +0300

    operators in Go

---
+++ operator.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+
+func main(){
+	//Arithmetic Operators
+	//addition
+	num1:= 79
+	num2:=56
+
+	sum:= num1 + num2
+	fmt.Printf("%d + %d=%d\n", num1, num2 ,sum)
+
+	//subtraction
+	difference:=num1-num2
+	fmt.Printf("%d -%d=%d\n", num1,num2, difference)
+
+	//division-integer
+	division:= num1/num2
+	fmt.Printf("%d/ %d =%d\n", num1,num2, division)
+
+	//modulous
+	remainder:= num1%num2
+	fmt.Println(remainder)
+
+	//incrteamental and decreamental
+	num1++
+	fmt.Println(num1)
+
+	num2--
+	fmt.Println(num2)
+}
