4042855e6b2b79fb921b48459466a78a2107aaaa
Muthonikiboi <kiboijoye254@gmail.com>
2023-10-22 23:15:44 +0300

    struct in go and accessing a sttruct

--- struct.go
+++ struct.go
@@ -1,2 +1,21 @@
 package main
 import "fmt"
+
+func main(){
+	type Person struct{
+		name string
+		course string 
+		age int
+	}
+
+person1 :=Person{"Joy","IT",21}
+fmt.Println(person1)
+
+
+var person2 Person
+person2=Person{"Manu","IT",22}
+fmt.Println(person2)
+
+//accessing a struct
+fmt.Println("Pewrson1's Name:",person1.name)
+}
