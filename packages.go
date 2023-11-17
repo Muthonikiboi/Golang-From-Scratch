0ed05863d08fdb1a90f8d16cd1cb8f0a3a1a595c
Muthonikiboi <kiboijoye254@gmail.com>
2023-11-17 23:49:06 +0300

    packages in golang

---
+++ packages.go
@@ -0,0 +1,16 @@
+//Go has three packages
+//fmt package 
+//string package
+//math package
+
+//fmt package
+
+package main
+import "fmt"
+
+func main(){
+	a:= 100
+	fmt.Println(a)
+}
+
+//string package
